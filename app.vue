ce4e51684a78e8364e9e6443f45d42de0584e805
Balazs Makrai <b.makrai@gmail.com>
2022-11-30 22:19:25 +0100

    add names

--- app.vue
+++ app.vue
@@ -6,36 +6,79 @@
       <div class="option-container">
         <h4>1) Choose a gender</h4>
         <div class="option-buttons">
-          <button class="option option-left" :class="(options.gender === 'Boy' && 'option-active')">Boy</button>
-          <button class="option" :class="(options.gender === 'Unisex' && 'option-active')">Unisex</button>
-          <button class="option option-right" :class="(options.gender === 'Girl' && 'option-active')">Girl</button>
+          <button class="option option-left" :class="(options.gender === Gender.BOY && 'option-active')"
+            @click="(options.gender = Gender.BOY)">Boy</button>
+          <button class="option" :class="(options.gender === Gender.UNISEX && 'option-active')"
+            @click="(options.gender = Gender.UNISEX)">Unisex</button>
+          <button class="option option-right" :class="(options.gender === Gender.GIRL && 'option-active')"
+            @click="(options.gender = Gender.GIRL)">Girl</button>
         </div>
       </div>
       <div class="option-container">
         <h4>2) Choose the name's popularity</h4>
         <div class="option-buttons">
-          <button class="option option-left" :class="(options.popularity === 'Trendy'&&'option-active')">Trendy</button>
-          <button class="option option-right" :class="(options.popularity === 'Unique'&&'option-active')">Unique</button>
+          <button class="option option-left" :class="(options.popularity === Popularity.TRENDY && 'option-active')"
+            @click="(options.popularity = Popularity.TRENDY)">Trendy</button>
+          <button class="option option-right" :class="(options.popularity === Popularity.UNIQUE && 'option-active')"
+            @click="(options.popularity = Popularity.UNIQUE)">Unique</button>
         </div>
       </div>
       <div class="option-container">
         <h4>3) Choose the name's lengthy</h4>
         <div class="option-buttons">
-          <button class="option option-left" :class="(options.length === 'Long'&&'option-active')">Long</button>
-          <button class="option" :class="(options.length === 'All'&&'option-active')">All</button>
-          <button class="option option-right" :class="(options.length === 'Short'&&'option-active')">Short</button>
+          <button class="option option-left" :class="(options.length === Length.LONG && 'option-active')"
+            @click="(options.length = Length.LONG)">Long</button>
+          <button class="option" :class="(options.length === Length.ALL && 'option-active')"
+            @click="(options.length = Length.ALL)">All</button>
+          <button class="option option-right" :class="(options.length === Length.SHORT && 'option-active')"
+            @click="(options.length = Length.SHORT)">Short</button>
         </div>
       </div>
+      <button class="primary" @click="computeSelectedNames">Find names</button>
     </div>
+    <div class="cards-container">
+      <div v-for="name in selectedNames" :key="name" class="card">
+        <h4>{{ name }}</h4>
+        <p>X</p>
+      </div>
+    </div>
+    {{ selectedNames }}
   </div>
 </template>
 
-<script setup>
-const options = reactive({
-  gender: "Unisex",
-  popularity: "Unique",
-  length: "Long"
+<script setup lang="ts">
+
+import { Gender, Popularity, Length, names } from '@/data';
+
+interface OptionsState {
+  gender: Gender;
+  popularity: Popularity;
+  length: Length;
+};
+
+const selectedNames = ref<string[]>([]);
+
+const options = reactive<OptionsState>({
+  gender: Gender.BOY,
+  popularity: Popularity.TRENDY,
+  length: Length.ALL
 });
+
+const computeSelectedNames = () => {
+  const filteredNames = names
+    .filter((name) => name.gender == options.gender)
+    .filter((name) => name.popularity === options.popularity)
+    .filter((name) => {
+      if (options.length === Length.ALL) {
+        return true;
+      }
+      else {
+        return name.length === options.length;
+      }
+    }
+    );
+  selectedNames.value = filteredNames.map(name => name.name);
+};
 </script>
 
 <style scoped>
@@ -88,5 +131,39 @@ h1 {
 .option-active {
   background-color: rgb(249, 87, 89);
   color: white;
+}
+
+.primary {
+  background-color: rgb(249, 87, 89);
+  color: white;
+  border-radius: 6.5rem;
+  border: none;
+  cursor: pointer;
+  padding: 0.75rem 4rem;
+  margin-top: 1rem;
+}
+
+.cards-container {
+  flex-wrap: wrap;
+  margin-top: 3rem;
+  display: flex;
+}
+
+.card {
+  background-color: rgb(27, 60, 138);
+  width: 28%;
+  color: white;
+  border-radius: 1rem;
+  padding: 1rem;
+  margin-right: 0.5rem;
+  margin-bottom: 1rem;
+}
+
+.card p {
+  position: absolute;
+  top: -30%;
+  left: 92.5%;
+  cursor: pointer;
+  color: rgba(255, 255, 255, 0.178)
 }
 </style>
