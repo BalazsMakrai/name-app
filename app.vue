fcdb0602015f1b039160e05fa1e3b2cbd8367e71
Balazs Makrai <b.makrai@gmail.com>
2022-12-01 10:37:56 +0100

    done project

--- app.vue
+++ app.vue
@@ -3,46 +3,13 @@
     <h1>Baby Name Generator</h1>
     <p>Choose your options and click 'Find names" button below</p>
     <div class="options-container">
-      <div class="option-container">
-        <h4>1) Choose a gender</h4>
-        <div class="option-buttons">
-          <button class="option option-left" :class="(options.gender === Gender.BOY && 'option-active')"
-            @click="(options.gender = Gender.BOY)">Boy</button>
-          <button class="option" :class="(options.gender === Gender.UNISEX && 'option-active')"
-            @click="(options.gender = Gender.UNISEX)">Unisex</button>
-          <button class="option option-right" :class="(options.gender === Gender.GIRL && 'option-active')"
-            @click="(options.gender = Gender.GIRL)">Girl</button>
-        </div>
-      </div>
-      <div class="option-container">
-        <h4>2) Choose the name's popularity</h4>
-        <div class="option-buttons">
-          <button class="option option-left" :class="(options.popularity === Popularity.TRENDY && 'option-active')"
-            @click="(options.popularity = Popularity.TRENDY)">Trendy</button>
-          <button class="option option-right" :class="(options.popularity === Popularity.UNIQUE && 'option-active')"
-            @click="(options.popularity = Popularity.UNIQUE)">Unique</button>
-        </div>
-      </div>
-      <div class="option-container">
-        <h4>3) Choose the name's lengthy</h4>
-        <div class="option-buttons">
-          <button class="option option-left" :class="(options.length === Length.LONG && 'option-active')"
-            @click="(options.length = Length.LONG)">Long</button>
-          <button class="option" :class="(options.length === Length.ALL && 'option-active')"
-            @click="(options.length = Length.ALL)">All</button>
-          <button class="option option-right" :class="(options.length === Length.SHORT && 'option-active')"
-            @click="(options.length = Length.SHORT)">Short</button>
-        </div>
-      </div>
+      <Option v-for="option in optionsArray" :key="option.title" :option="option" :options="options" />
       <button class="primary" @click="computeSelectedNames">Find names</button>
     </div>
     <div class="cards-container">
-      <div v-for="name in selectedNames" :key="name" class="card">
-        <h4>{{ name }}</h4>
-        <p>X</p>
-      </div>
+      <CardName v-for="(name, index) in selectedNames" :key="name" :name="name" @remove="()=>removeName(index)"
+        :index="index" />
     </div>
-    {{ selectedNames }}
   </div>
 </template>
 
@@ -64,6 +31,26 @@ const options = reactive<OptionsState>({
   length: Length.ALL
 });
 
+const removeName = (index: number) => {
+  const filteredNames = [...selectedNames.value];
+  filteredNames.splice(index, 1);
+  selectedNames.value = filteredNames;
+};
+const optionsArray = [
+  {
+    title: '1) Choose a gender',
+    category: "gender",
+    buttons: [Gender.GIRL, Gender.UNISEX, Gender.BOY]
+  }, {
+    title: '2) Choose the popularity of the name',
+    category: "popularity",
+    buttons: [Popularity.TRENDY, Popularity.UNIQUE]
+  }, {
+    title: '3) Choose the length of the name',
+    category: "length",
+    buttons: [Length.SHORT, Length.ALL, Length.LONG]
+  },
+];
 const computeSelectedNames = () => {
   const filteredNames = names
     .filter((name) => name.gender == options.gender)
@@ -104,35 +91,6 @@ h1 {
   position: relative;
 }
 
-.option-container {
-  margin-bottom: 2rem;
-}
-
-.option {
-  cursor: pointer;
-  font-weight: 200;
-  background-color: white;
-  outline: 0.15rem solid rgb(249, 87, 89);
-  border: none;
-  padding: 0.75rem;
-  width: 12rem;
-  font-size: 1rem;
-  color: rgb(27, 60, 138);
-}
-
-.option-left {
-  border-radius: 1rem 0 0 1rem;
-}
-
-.option-right {
-  border-radius: 0 1rem 1rem 0;
-}
-
-.option-active {
-  background-color: rgb(249, 87, 89);
-  color: white;
-}
-
 .primary {
   background-color: rgb(249, 87, 89);
   color: white;
@@ -147,23 +105,5 @@ h1 {
   flex-wrap: wrap;
   margin-top: 3rem;
   display: flex;
-}
-
-.card {
-  background-color: rgb(27, 60, 138);
-  width: 28%;
-  color: white;
-  border-radius: 1rem;
-  padding: 1rem;
-  margin-right: 0.5rem;
-  margin-bottom: 1rem;
-}
-
-.card p {
-  position: absolute;
-  top: -30%;
-  left: 92.5%;
-  cursor: pointer;
-  color: rgba(255, 255, 255, 0.178)
 }
 </style>
